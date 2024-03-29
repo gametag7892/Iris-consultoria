cf2105b43f77eb613d7960cded41ca1be61bbba9
gametag7892 <78163768+gametag7892@users.noreply.github.com>
2024-03-29 18:15:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
-const myCarouselElement = document.querySelector('#myCarousel')
-
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 2000,
-  touch: false
-})
